5ef7e89d9b18a5fb71fab6738b9c3a951370a52d
BeckXuan <36814840+BeckXuan@users.noreply.github.com>
2020-12-19 22:06:00 +0800

    添加用户操作函数

--- common.php
+++ common.php
@@ -24,3 +24,74 @@ function filter($str)
     //$str = htmlspecialchars($str); // html标记转换
     return $str;
 }
+
+function &sqlQuery($sql)
+{
+    $conn = &getDBConn();
+    $result = $conn->query($sql);
+    return $result;
+}
+
+function studentIDExists($studentID, $needFilter = true)
+{
+    if ($needFilter) {
+        $studentID = filter($studentID);
+    }
+    $result = &sqlQuery("select * from user where studentID='$studentID' limit 1");
+    return boolval($result->num_rows);
+}
+
+function addUser($name, $studentID, $password, $needMD5 = true, $needFilter = true)
+{
+    if ($needFilter) {
+        $name = filter($name);
+        $studentID = filter($studentID);
+    }
+    if ($needMD5) {
+        $password = md5($password);
+    }
+    return sqlQuery("INSERT INTO user(name, studentID, password, date) VALUES ('$name', '$studentID', '$password', CURDATE())");
+}
+
+function admitUser($studentID, $needFilter = true)
+{
+    if ($needFilter) {
+        $studentID = filter($studentID);
+    }
+    return sqlQuery("UPDATE user SET admitted=1 WHERE studentID='$studentID'");
+}
+
+function denyUser($studentID, $needFilter = true)
+{
+    if ($needFilter) {
+        $studentID = filter($studentID);
+    }
+    return sqlQuery("UPDATE user SET admitted=0 WHERE studentID='$studentID'");
+}
+
+function delUser($studentID, $needFilter = true)
+{
+    if ($needFilter) {
+        $studentID = filter($studentID);
+    }
+    return sqlQuery("DELETE FROM user WHERE studentID='$studentID'");
+}
+
+function changeUserName($studentID, $name, $needFilter = true)
+{
+    if ($needFilter) {
+        $studentID = filter($studentID);
+    }
+    return sqlQuery("UPDATE user SET name='$name' WHERE studentID='$studentID'");
+}
+
+function changeUserPassword($studentID, $password, $needMD5 = true, $needFilter = true)
+{
+    if ($needFilter) {
+        $studentID = filter($studentID);
+    }
+    if ($needMD5) {
+        $password = md5($password);
+    }
+    return sqlQuery("UPDATE user SET password='$password' WHERE studentID='$studentID'");
+}
