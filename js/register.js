dd136a78d4d74bf230e228ae25d9f92e40217f6f
BeckXuan <36814840+BeckXuan@users.noreply.github.com>
2020-12-21 18:11:33 +0800

    注册流程美化... 实时提示是否重名...

--- js/register.js
+++ js/register.js
@@ -1,56 +1,79 @@
 let reg_form = document.getElementsByName('register')[0];
 let reg_name = reg_form['name']
 let reg_studentID = reg_form['studentID']
-let last_text = {
-    'name' : '',
-    'studentID': ''
+let timeId = {
+    'name': null,
+    'studentID': null
+}
+let XHR = {
+    'name': null,
+    'studentID': null
+}
+let sending = {
+    'name': false,
+    'studentID': false
 }
 
 function isEmpty(text) {
     return text === undefined || text === "" || text === null
 }
 
-reg_name.addEventListener("blur", () => {
-    validate('name', reg_name)
-}, false)
-reg_studentID.addEventListener("blur", () => {
-    validate('studentID', reg_studentID)
-}, false)
-
 function validate(type, doc_text) {
-    let value = doc_text.value
-    if (last_text[type] === value) {
-        return;
+    if (timeId[type]) {
+        clearTimeout(timeId[type])
+        timeId[type] = null
     }
-    if (isEmpty(value)) {
-        reg_name.className = ''
-        return
+    if (sending[type]) {
+        XHR[type].abort()
+        sending[type] = false
     }
-    last_text[type] = value;
-    let xmlHttp;
-    if (window.XMLHttpRequest) {// IE7+, Firefox, Chrome, Opera, Safari 代码
-        xmlHttp = new XMLHttpRequest();
-    } else {// IE6, IE5 代码
-        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState === 4) {
-            if (xmlHttp.status === 200) {
-                //success
-                doc_text.className = ''
-            } else if (xmlHttp.status === 422) {
-                //error
-                doc_text.className = 'check-error'
-                alert(xmlHttp.responseText)
-            } else {
-                //fail
-                alert('服务器连接失败！')
+    doc_text.className = ''
+    timeId[type] = setTimeout(() => {
+        timeId[type] = null
+        let value = doc_text.value
+        if (isEmpty(value)) {
+            return
+        }
+        doc_text.className = 'check-process'
+        let xhr;
+        if (XHR[type]) {
+            xhr = XHR[type]
+        } else {
+            if (window.XMLHttpRequest) {// IE7+, Firefox, Chrome, Opera, Safari 代码
+                xhr = new XMLHttpRequest();
+            } else {// IE6, IE5 代码
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            XHR[type] = xhr
+            xhr.onload = function () {
+                sending[type] = false
+                if (xhr.status === 200) {
+                    //success
+                    doc_text.className = ''
+                } else if (xhr.status === 422) {
+                    //error
+                    doc_text.className = 'check-error'
+                    //alert(xhr.responseText)
+                } else {
+                    //fail
+                    //alert('服务器连接失败！')
+                }
+            }
+            xhr.timeout = 2000;
+            xhr.ontimeout = function () {
+                alert('请求服务器超时！')
             }
         }
-    }
-    xmlHttp.open("POST", 'register_check/' + type + '.php', true);
-    xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xmlHttp.send('value=' + value);
+        xhr.open("POST", 'register_check/' + type + '.php', true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.send('value=' + value);
+        sending[type] = true
+    }, 500)
+}
+
+function resetAll() {
+    reg_name.className = ''
+    reg_studentID.className = ''
 }
 
 function _register() {
