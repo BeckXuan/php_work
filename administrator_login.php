2ff942fc17e465e44c63bf56463aec8da3c754fa
xzj-x <56643138+xzj-x@users.noreply.github.com>
2020-12-23 21:50:41 +0800

    添加管理员登录界面 及 管理员学生界面跳转按钮功能

--- administrator_login.php
+++ administrator_login.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['studentID'], $_COOKIE['studentID']) && $_SESSION['studentID
 <html lang="zh-CN">
 <head>
     <meta charset="utf-8">
-    <title>登录注册页面</title>
+    <title>管理员登录</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
     <link rel="icon" sizes="any" href="images/favicon.ico">
@@ -18,9 +18,9 @@ if (isset($_SESSION['studentID'], $_COOKIE['studentID']) && $_SESSION['studentID
 <div class="content">
     <form id="login">
         <div class="form sign-in">
-            <h2>欢迎回来！同学们</h2>
+            <h2>管理员登录</h2>
             <label>
-                <span>学号</span>
+                <span>管理员账号</span>
                 <input type="text" name="studentID" required/>
             </label>
             <label>
@@ -31,50 +31,27 @@ if (isset($_SESSION['studentID'], $_COOKIE['studentID']) && $_SESSION['studentID
                 <span>记住我</span>
                 <input type="checkbox" name="rem">
             </label>
-            <p class="forgot-pass"><a href="javascript:">忘记密码？</a></p>
+            <br/><br/>
             <button type="submit" class="submit" id="btn_log">登 录</button>
-            <button type="button" class="fb-btn" onclick="window.location.href='administrator_login.php'">使用 <span>管理员</span> 帐号登录</button>
+            <button type="button" class="fb-btn" onclick="window.location.href='login.php'">使用 <span>学生</span> 帐号登录
+            </button>
         </div>
     </form>
     <div class="sub-cont">
         <div class="img">
             <div class="img__text m--up">
-                <h2>还未注册？</h2>
-                <br>
-                <p>立即注册</p>
-                <br>
-                <p>与老师同学们共聚一堂</p>
+                <h2>欢迎回来！</h2>
+                <br/><br/><br/>
+                <p>亲爱的管理员先生/女士！</p>
             </div>
             <div class="img__text m--in">
                 <h2>已有帐号？</h2>
                 <br>
                 <p>快来登录吧，好久不见了！</p>
             </div>
-            <div class="img__btn">
-                <span class="m--up">注 册</span>
-                <span class="m--in">登 录</span>
-            </div>
+
         </div>
-        <form id="register">
-            <div class="form sign-up">
-                <h2>立即注册</h2>
-                <label>
-                    <span id="sp_name">用户名</span>
-                    <input type="text" name="name" required/>
-                </label>
-                <label>
-                    <span id="sp_studentID">学号</span>
-                    <input type="text" name="studentID" required/>
-                </label>
-                <label>
-                    <span id="sp_password">密码</span>
-                    <a id="passwordEye"></a>
-                    <input type="password" name="password" required/>
-                </label>
-                <button type="submit" class="submit" id="btn_reg">注 册</button>
-                <button type="reset" class="fb-btn" id="btn_rst"><span>重 置</span></button>
-            </div>
-        </form>
+
     </div>
 </div>
 
