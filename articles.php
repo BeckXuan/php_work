f329ca94667ff8c3e5238f2b36f85fbbd10c2006
BeckXuan <36814840+BeckXuan@users.noreply.github.com>
2020-12-27 16:51:58 +0800

    小修小改...

--- articles.php
+++ articles.php
@@ -7,7 +7,7 @@ require "common.php";
 //}
 
 $db = DB::getInstance();
-$db->initArticleInformation(0, 10, true);
+$db->initArticleInformation(0, -1, true);
 ?>
 <html lang="zh-CN">
 <head>
@@ -21,48 +21,33 @@ $db->initArticleInformation(0, 10, true);
         <a href="#" target="_self">全部文章</a>
     </div>
     <div class="newsCenterPanel_inner">
-        <?php
+<?php
+        $class = '';
         while ($article = $db->getNextArticle()) {
-            $year_month = substr($article->getTime(), 0, 7);
-            $day = substr($article->getTime(), 9, 2);
+            $href = 'article_show.php?id=' . $article->getId();
+            $title = $article->getTitle();
+            $content = $article->getContent();
+            $time = strtotime($article->getTime());
+            $year_month = date('Y-m', $time);
+            $day = date('d', $time);
+            $class = $class === 'odd' ? 'even' : 'odd';
             echo <<<html
-        <div class="newContentBox odd">
+        <div class="newContentBox {$class}">
             <a href="#">
                 <div class="time">
                     <p class="day">{$day}</p>
                     <p class="ym">{$year_month}</p>
                 </div>
-                <div class="newTitle">{$article->getTitle()}</div>
+                <div class="newTitle">{$title}</div>
                 <div class="newTitleIcon"></div>
                 <div class="border"></div>
                 <div class="newContent">
-                    {$article->getContent()}
+                    {$content}
                 </div>
             </a>
         </div>
 html;
-            if ($article = $db->getNextArticle()) {
-                $year_month = substr($article->getTime(), 0, 7);
-                $day = substr($article->getTime(), 9, 2);
-                echo <<<html
-        <div class="newContentBox even">
-            <a href="#">
-                <div class="time">
-                    <p class="day">{$day}</p>
-                    <p class="ym">{$year_month}</p>
-                </div>
-                <div class="newTitle">{$article->getTitle()}</div>
-                <div class="newTitleIcon"></div>
-                <div class="border"></div>
-                <div class="newContent">
-                    {$article->getContent()}
-                </div>
-            </a>
-        </div>
-html;
-            }
-        }
-        ?>
+        } ?>
     </div>
 </div>
 </body>
