0d1b93386785b115da348d4f370f7c583e9fb9f5
BeckXuan <36814840+BeckXuan@users.noreply.github.com>
2020-12-27 13:27:01 +0800

    测试文章和留言数量增至5个...

--- init_environment/2_create_test_data.php
+++ init_environment/2_create_test_data.php
@@ -10,14 +10,18 @@ $db->addUser('null', '05', 'test');
 $db->admitUser('01');
 $db->admitUser('02');
 $db->admitUser('03');
-$db->admitUser('04');
 echo '创建测试用户命令已执行完成...<br/>';
 
 $db->addArticle('testArticle1', 'testContent1');
 $db->addArticle('testArticle2', 'testContent2');
 $db->addArticle('testArticle3', 'testContent3');
+$db->addArticle('testArticle4', 'testContent4');
+$db->addArticle('testArticle5', 'testContent5');
 echo '创建测试文章命令已执行完成....<br/>';
 
 $db->addMessage(1, 'testComment1', '01');
-$db->addMessage(1, 'testComment2', '01');
+$db->addMessage(1, 'testComment2', '02');
+$db->addMessage(1, 'testComment3', '03');
+$db->addMessage(1, 'testComment4', '04');
+$db->addMessage(1, 'testComment5', '05');
 echo '创建测试留言命令已执行完成....<br/>';
