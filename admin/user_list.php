778a2add5ddbcf3112a98cf5ef9b6dc846efdedd
BeckXuan <36814840+BeckXuan@users.noreply.github.com>
2020-12-30 12:18:41 +0800

    修复bug...

--- admin/user_list.php
+++ admin/user_list.php
@@ -86,7 +86,7 @@ tr;
     </div>
 </div>
 <!--添加用户图层-->
-<div class="add_menber" id="menber_style" style="display:none">
+<div class="add_member" id="member_style" style="display:none">
     <ul class="page-content">
         <li><label class="label_name">用&nbsp;&nbsp;户 &nbsp;名：<span class="add_name"><input value="" name="name"
                                                                                            type="text"
@@ -128,26 +128,26 @@ tr;
     })
     /*用户-添加*/
     $('#member_add').on('click', function () {
-        let input_name = $("#menber_style input[name$='name']")
-        let input_studentID = $("#menber_style input[name$='studentID']")
-        let input_password = $("#menber_style input[name$='password']")
+        let input_name = $("#member_style input[name$='name']")
+        let input_studentID = $("#member_style input[name$='studentID']")
+        let input_password = $("#member_style input[name$='password']")
         input_name.val('')
         input_studentID.val('')
         input_password.val('')
-        $("#menber_style input[type='radio']:first").attr('checked', 'checked');
+        $("#member_style input[type='radio']:first").attr('checked', 'checked');
         layer.open({
             type: 1,
             title: '添加用户',
             shadeClose: true, //点击遮罩关闭层
             area: ['280px', ''],
-            content: $('#menber_style'),
+            content: $('#member_style'),
             btn: ['提交', '取消'],
             yes: function (index) {
 
                 let name = input_name.val()
                 let studentID = input_studentID.val()
                 let password = hex_md5(input_password.val())
-                let admitted = $("#menber_style input[type='radio']:checked").val();
+                let admitted = $("#member_style input[type='radio']:checked").val();
                 if (name === '' || studentID === '' || password === '') {
                     layer.alert("所填信息不能为空！", {
                         title: '提示框',
@@ -162,12 +162,12 @@ tr;
                     input_password.val('')
                     if (admitted) {
                         _request('operate/admitUser.php', 'value=' + studentID, () => {
-                            layer.alert('添加并启用成功！', {title: '提示框', icon: 1})
+                            layer.alert('添加并启用成功！(刷新后显示)', {title: '提示框', icon: 1})
                         }, (xhr) => {
-                            layer.alert('添加成功但启用失败！' + xhr.responseText, {title: '提示框', icon: 2})
+                            layer.alert('添加成功但启用失败！(刷新后显示)' + xhr.responseText, {title: '提示框', icon: 2})
                         })
                     } else {
-                        layer.alert('添加成功！', {title: '提示框', icon: 1})
+                        layer.alert('添加成功！(刷新后显示)', {title: '提示框', icon: 1})
                     }
                 }, (xhr) => {
                     layer.alert('添加失败！' + xhr.responseText, {title: '提示框', icon: 2})
@@ -185,7 +185,7 @@ tr;
             _request('operate/denyUser.php', 'value=' + id, () => {
                 layer.msg('已停用!', {icon: 1, time: 2000});
                 tr.find(".td-manage").prepend('<a style="text-decoration:none" class="btn btn-xs " onClick="member_start(this)" href="javascript:;" title="启用"><i class="icon-ok bigger-120"></i></a>');
-                tr.find(".td-status").html('<span class="label label-defaunt radius">已停用</span>');
+                tr.find(".td-status").html('<span class="label label-default radius">已停用</span>');
                 _obj.remove();
             }, (xhr) => {
                 layer.msg('停用失败！' + xhr.responseText, {icon: 2, time: 3000})
@@ -213,9 +213,9 @@ tr;
     /*用户-编辑*/
     function member_edit(obj) {
         let _obj = $(obj)
-        let input_name = $("#menber_style input[name$='name']")
-        let input_studentID = $("#menber_style input[name$='studentID']")
-        let input_password = $("#menber_style input[name$='password']")
+        let input_name = $("#member_style input[name$='name']")
+        let input_studentID = $("#member_style input[name$='studentID']")
+        let input_password = $("#member_style input[name$='password']")
         let td = _obj.parent('td').siblings('td')
         let td_ID = td.eq(1)
         let td_name = td.eq(2)
@@ -227,10 +227,10 @@ tr;
         let originAdmitted = 0
         if (status === '已启用') {
             originAdmitted = '1'
-            $("#menber_style input[type='radio']:first").attr('checked', 'checked');
+            $("#member_style input[type='radio']:first").attr('checked', 'checked');
         } else if (status === '已停用') {
             originAdmitted = '-1'
-            $("#menber_style input[type='radio']:last").attr('checked', 'checked');
+            $("#member_style input[type='radio']:last").attr('checked', 'checked');
         }
         input_name.val(originName)
         input_studentID.val(originID)
@@ -240,13 +240,13 @@ tr;
             title: '修改信息(留空不修改)',
             shadeClose: true, //点击遮罩关闭层
             area: ['280px', ''],
-            content: $('#menber_style'),
+            content: $('#member_style'),
             btn: ['提交', '取消'],
             yes: function (index) {
                 let name = input_name.val()
                 let studentID = input_studentID.val()
                 let password = input_password.val()
-                let admitted = $("#menber_style input[type='radio']:checked").val();
+                let admitted = $("#member_style input[type='radio']:checked").val();
                 if ((name === '' || originName === name) && (studentID === '' || originID === studentID) && password === '' && originAdmitted === admitted) {
                     layer.alert("未修改任何信息！", {
                         title: '提示框',
@@ -286,7 +286,7 @@ tr;
                     } else if (admitted === '-1') {
                         td_manage.children('a').eq(0).remove()
                         td_manage.prepend('<a style="text-decoration:none" class="btn btn-xs " onClick="member_start(this)" href="javascript:;" title="启用"><i class="icon-ok bigger-120"></i></a>');
-                        td_status.html('<span class="label label-defaunt radius">已停用</span>');
+                        td_status.html('<span class="label label-default radius">已停用</span>');
                     }
                 }, (xhr) => {
                     layer.alert('错误！' + xhr.responseText, {
@@ -301,9 +301,10 @@ tr;
     /*用户-删除*/
     function member_del(obj) {
         layer.confirm('确认要删除吗？', function () {
-            let id = tr.children('td').eq(1).text()
+            let _obj = $(obj)
+            let id = _obj.parent("td").siblings().eq(1).text()
             _request('operate/delUser.php', 'value=' + id, () => {
-                $(obj).parents("tr").remove()
+                _obj.parents("tr").remove()
                 layer.msg('已删除!', {icon: 1, time: 2000})
             }, (xhr) => {
                 layer.msg('删除失败！' + xhr.responseText, {icon: 2, time: 3000})
